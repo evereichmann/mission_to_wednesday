d4e571d996b4f8d869a6595d670dd07e115d2a3f
Simon51584 <simon51584@gmail.com>
2020-07-22 16:54:27 -0500

    updated missions CRUD

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -1,5 +1,43 @@
 class MissionsController < ApplicationController
+    #index
     def index
+        @missions = Mission.all
+    end    
+    #show
+    def show
+        @mission = Mission.find(params[:id])  
+    end    
+    #new
+    def new
+        @mission = Mission.new
+    end    
+    #create
+    def create
+        @mission = Mission.create(mission_params)
+        redirect_to mission_path(@missions)
+    end    
+    #edit
+    def edit
+        @mission = Mission.find(params[:id])
+    end    
+    #update
+    def update 
+        @mission = Mission.find(params[:id])
+        @mission.update(mission_params)
+        redirect_to mission_path(@missions)
+    end    
+    #delete
+    def destroy
+        @missions = Mission.find(params[:id])
+        @missions.destroy
+        redirect_to mission_path
+    end    
+    ##private
+    #mission_params
+    private
 
-    end
+    def mission_params
+        params.require(:mission).permit(:name, :date, :plan)
+    end    
+
 end
--- app/controllers/planets_controller.rb
+++ app/controllers/planets_controller.rb
@@ -1,2 +1,10 @@
 class PlanetsController < ApplicationController
+    #index
+    def index
+        @missions = Mission.all
+    end    
+    #show
+    def show
+        @mission = Mission.find(params[:id])  
+    end    
 end
